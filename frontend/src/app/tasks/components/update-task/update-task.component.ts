8d3e9676f503ad58aa20e6cf4b304168a68d0453
salma-mohamed-37 <lulymohamed37@gmail.com>
2023-09-30 21:49:28 +0300

    the structure, styling,form of update component

--- frontend/src/app/tasks/components/update-task/update-task.component.ts
+++ frontend/src/app/tasks/components/update-task/update-task.component.ts
@@ -1,34 +1,34 @@
 import { Component } from '@angular/core';
-import { Task} from '../../interfaces/Task';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Task } from '../../interfaces/Task';
 
 @Component({
-  selector: 'app-add-task',
-  templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  selector: 'app-update-task',
+  templateUrl: './update-task.component.html',
+  styleUrls: ['./update-task.component.css']
 })
-export class AddTaskComponent
+export class UpdateTaskComponent
 {
-  addForm! :FormGroup;
+  updateForm! :FormGroup;
   constructor(private fb: FormBuilder){}
 
   ngOnInit() {
-    this.addForm = this.fb.group({
+    this.updateForm = this.fb.group({
       content : this.fb.control('',Validators.required),
       dueDate : this.fb.control('',Validators.required)
     });
   }
+
   submit()
   {
     let task : Task;
     task = {
       'id':0,
-      'content' : this.addForm.get('content')!.value,
-      'dueDate' : new Date(this.addForm.get('dueDate')!.value),
+      'content' : this.updateForm.get('content')!.value,
+      'dueDate' : new Date(this.updateForm.get('dueDate')!.value),
       'isCompleted' : false
     }
     console.log(task)
   }
-
 
 }
